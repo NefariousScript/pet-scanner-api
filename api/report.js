5e7799464438e83f91ea615c296b45b775fbb5d7
NefariousScript <tempest0945@gmail.com>
2026-07-04 07:03:07 +0800

    Update report.js

--- api/report.js
+++ api/report.js
@@ -1,48 +1,6 @@
-import { supabase } from "../lib/supabase.js";
-
 export default async function handler(req, res) {
 
+    console.log("METHOD:", req.method);
+    console.log("BODY:", req.body);
+
     if (req.method === "POST") {
-        const { scannerId, species, serverId } = req.body;
-
-        const { error } = await supabase
-            .from("reports")
-            .insert([{ scannerId, species, serverId }]);
-
-        if (error) {
-            return res.status(500).json({
-                success: false,
-                error: error.message
-            });
-        }
-
-        return res.status(200).json({
-            success: true,
-            message: "Saved to Supabase"
-        });
-    }
-
-    if (req.method === "GET") {
-        const { data, error } = await supabase
-            .from("reports")
-            .select("*")
-            .order("id", { ascending: false });
-
-        if (error) {
-            return res.status(500).json({
-                success: false,
-                error: error.message
-            });
-        }
-
-        return res.status(200).json({
-            success: true,
-            data
-        });
-    }
-
-    return res.status(405).json({
-        success: false,
-        message: "Method not allowed"
-    });
-} 
